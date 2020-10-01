0f0163e29e2f7c4c339ec19b9fe0c13fd897155f
Lukasz <lukjut@gmail.com>
2020-10-01 13:53:54 +0300

    Init commit 2 return of the king (added Standard Scaffold)

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # healthy_food_recipes
 
-2nd task for Hybrid Mobile Applications lecture
+2nd task for Hybrid Mobile Applications lecture.
 
-Task is to create a hybrid mobile application that provides user with various food recipes
+Task is to create a hybrid mobile application that provides user with various food recipes.
 User should be able to add multiple comments to the recipe.
 
 # Built with
--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,29 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Healthy Food Recipes',
-      home: Scaffold(
+      theme: ThemeData(
+        primaryColor: Colors.yellow,
+      ),
+      home: HomePage(),
+    );
+
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text('Healthy Food Recipes'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    ),
+    body: null,
     );
-    
   }
 }
